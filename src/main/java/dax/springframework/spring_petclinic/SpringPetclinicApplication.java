134932af42dff26afc6d534dea3c2e696bb8a380
Dax Wann <daxwann@gmail.com>
2020-02-23 17:07:17 -0800

    initialize project

---
+++ src/main/java/dax/springframework/spring_petclinic/SpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package dax.springframework.spring_petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringPetclinicApplication.class, args);
+  }
+
+}
