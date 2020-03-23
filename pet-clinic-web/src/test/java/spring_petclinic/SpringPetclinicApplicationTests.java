3fa91680668af02f82eb1ebd7a637cc7ff2a7fa7
Dax Wann <daxwann@gmail.com>
2020-03-22 19:45:43 -0700

    add pet-clinic-data and pet-clinic-web modules. closes #1

--- pet-clinic-web/src/test/java/spring_petclinic/SpringPetclinicApplicationTests.java
+++ pet-clinic-web/src/test/java/spring_petclinic/SpringPetclinicApplicationTests.java
@@ -1,9 +1,10 @@
-package dax.springframework.spring_petclinic;
+package spring_petclinic;
 
+import dax.springframework.spring_petclinic.SpringPetclinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringPetclinicApplication.class)
 class SpringPetclinicApplicationTests {
 
   @Test
