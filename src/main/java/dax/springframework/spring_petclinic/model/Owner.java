92fbebec16f56920494a2f08f3a7a5e6ab118212
Dax Wann <daxwann@gmail.com>
2020-03-22 18:20:54 -0700

    add POJOs. Closes #2

---
+++ src/main/java/dax/springframework/spring_petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package dax.springframework.spring_petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/dax/springframework/spring_petclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package dax.springframework.spring_petclinic.model;
+
+public class PetType {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
