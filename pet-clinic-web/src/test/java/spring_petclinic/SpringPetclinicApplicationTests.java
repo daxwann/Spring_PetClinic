149254e29c8f116c4cef2e43e406da8c0ec1dcf9
Dax Wann <daxwann@gmail.com>
2020-03-28 13:09:06 -0700

    fix test config

--- pet-clinic-web/src/test/java/spring_petclinic/SpringPetclinicApplicationTests.java
+++ pet-clinic-web/src/test/java/spring_petclinic/SpringPetclinicApplicationTests.java
@@ -2,8 +2,11 @@ package spring_petclinic;
 
 import dax.springframework.spring_petclinic.SpringPetclinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = SpringPetclinicApplication.class)
 class SpringPetclinicApplicationTests {
 
