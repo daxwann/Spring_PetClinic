45506317c832e6109514566336d2ae1adb21d7cb
Dax Wann <daxwann@gmail.com>
2020-04-05 14:50:15 -0700

    refactor service interfaces to inherit from CrudService. Closes #11.

--- pet-clinic-data/src/main/java/dax/springframework/spring_petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/dax/springframework/spring_petclinic/services/OwnerService.java
@@ -2,14 +2,6 @@ package dax.springframework.spring_petclinic.services;
 
 import dax.springframework.spring_petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-  Owner findById(Long id);
-
+public interface OwnerService extends CrudService<Owner, Long> {
   Owner findByLastName(String lastName);
-
-  Owner save(Owner owner);
-
-  Set<Owner> findAll();
 }
